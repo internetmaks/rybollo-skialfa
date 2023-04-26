6ee538bb50b3dddb1f15d8c050de140edc6ae8f5
internetmaks <131857685+internetmaks@users.noreply.github.com>
2023-04-26 10:17:08 +0200

    Update zaj13.py

--- zaj13.py
+++ zaj13.py
@@ -1,9 +1,9 @@
-# def dodawanie(liczba1, liczba2):
-#     suma=liczba1+liczba2
-#     return suma
-# def mnozenie(liczba1,liczba2):
-#     iloczyn=liczba1*liczba2
-#     return iloczyn
+ def dodawanie(liczba1, liczba2):
+     suma=liczba1+liczba2
+     return suma
+ def mnozenie(liczba1,liczba2):
+     iloczyn=liczba1*liczba2
+        return iloczyn
 
 # print('podaj liczbe')
 # pierwsza=int(input())
